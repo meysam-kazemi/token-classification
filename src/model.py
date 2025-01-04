158c483163b7fa29e171f51411cb950a8cda1e7f
meysam-kazemi <meisam.daysu@gmail.com>
2025-01-04 05:24:26 +0330

    oop

--- src/model.py
+++ src/model.py
@@ -4,44 +4,42 @@ import os
 from transformers import AutoModelForTokenClassification, AutoTokenizer
 from utils import read_config
 
-def load_and_save_model(model_name, save_dir):
-    """
-    Loads a pre-trained model and tokenizer, and saves them to the specified directory.
 
-    Args:
-        model_name (str): The name of the pre-trained model to load (e.g., 'bert-base-uncased').
-        save_dir (str): The directory where the model and tokenizer will be saved.
-    """
-    # Create the save directory if it doesn't exist
-    os.makedirs(save_dir, exist_ok=True)
+class modelAndTokenizer:
+    def __init__(self):
+        config = read_config()
+        self.save_dir = config['model']['save_dir']
+        self.model_name = config['model']['name']
+        try:
+            self._load_model_and_tokenizer_locally()
+            print("Locally")
+        except:
+            self._load_and_save_model()
+    def _load_and_save_model(self):
+        """
+        Loads a pre-trained model and tokenizer, and saves them to the specified directory.
+        """
+        # Create the save directory if it doesn't exist
+        os.makedirs(self.save_dir, exist_ok=True)
 
-    # Load the model and tokenizer
-    model = AutoModelForTokenClassification.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+        # Load the model and tokenizer
+        self.model = AutoModelForTokenClassification.from_pretrained(self.model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
 
-    # Save the model and tokenizer
-    model.save_pretrained(save_dir)
-    tokenizer.save_pretrained(save_dir)
+        # Save the model and tokenizer
+        model.save_pretrained(self.save_dir)
+        tokenizer.save_pretrained(self.save_dir)
 
-    print(f"Model and tokenizer for '{model_name}' saved in '{save_dir}'.")
+        print(f"Model and tokenizer for '{self.model_name}' saved in '{self.save_dir}'.")
 
+    def _load_model_and_tokenizer_locally(self):
+        """
+        Loads the model and tokenizer from the specified directory.
+        """
 
-def load_model_and_tokenizer_locally(model_dir):
-    """
-    Loads the model and tokenizer from the specified directory.
-
-    Args:
-        model_dir (str): The directory where the model and tokenizer are saved.
-
-    Returns:
-        model: The loaded model.
-        tokenizer: The loaded tokenizer.
-    """
-    # Load the model and tokenizer from the specified directory
-    model = AutoModelForTokenClassification.from_pretrained(model_dir)
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
-
-    return model, tokenizer
+        # Load the model and tokenizer from the specified directory
+        self.model = AutoModelForTokenClassification.from_pretrained(self.save_dir)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.save_dir)
 
 if __name__=="__main__":
     config = read_config()
@@ -49,7 +47,4 @@ if __name__=="__main__":
     #     model_name=config['model']['name'],
     #     save_dir=config['model']['save_dir']
     #     )
-    model, tokenizer = load_model_and_tokenizer_locally(
-        config['model']['save_dir']
-    )
-    print(tokenizer.is_fast)
+    model_tokenizer = modelAndTokenizer()
