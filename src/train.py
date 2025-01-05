cedea0d6e891df88a50950d47425c966e85ca4a2
meysam-kazemi <meisam.daysu@gmail.com>
2025-01-06 03:29:26 +0330

    preparing for train loop

--- src/train.py
+++ src/train.py
@@ -1,4 +1,7 @@
 from torch.utils.data import DataLoader
+from torch.optim import AdamW
+from accelerate import Accelerator
+from transformers import get_scheduler
 from model import modelTokenizer
 from utils import (
     read_config,
@@ -6,6 +9,7 @@ from utils import (
     preProcessingTokens,
     postprocess,
 )
+
 
 config = read_config()
 data = load_saved_dataset(config['dataset']['name'])
@@ -25,3 +29,22 @@ eval_dataloader = DataLoader(
     tokenized_datasets["validation"], collate_fn=preprocess.data_collator, batch_size=8
 )
 
+optimizer = AdamW(mt.model.parameters(), lr=2e-5)
+
+
+accelerator = Accelerator()
+model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
+    mt.model, optimizer, train_dataloader, eval_dataloader
+)
+
+
+num_train_epochs = 3
+num_update_steps_per_epoch = len(train_dataloader)
+num_training_steps = num_train_epochs * num_update_steps_per_epoch
+
+lr_scheduler = get_scheduler(
+    "linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
