e58601077c348608fffecfb91519e691250ffa37
meysam-kazemi <meisam.daysu@gmail.com>
2025-01-06 06:18:50 +0330

    gradio for demo

---
+++ app.py
@@ -0,0 +1,19 @@
+from src.classifier import Classifer
+import gradio as gr
+
+classifier = Classifer()
+
+def chat(text, history):
+    history = history or []
+    history.append((text, classifier(text)))
+    return history, history
+
+
+iface = gr.Interface(
+    chat,
+    ["text", 'state'],
+    ["chatbot", 'state'],
+    # allow_screenshot=False,
+    allow_flagging="never",
+)
+iface.launch()
--- src/classifier.py
+++ src/classifier.py
@@ -1,31 +1,35 @@
 # src/classifier.py
-
 from transformers import pipeline
-from utils import read_config
+from src.utils import read_config
 
-config = read_config()
-model_checkpoint = config['model']['save_dir']
-token_classifier = pipeline(
-    "token-classification", model=model_checkpoint, aggregation_strategy="simple"
-)
-def tabluar_result(res):
-    output_text = ""
-    words = [ent['word'].center(10) for ent in res]
-    groups = [ent['entity_group'].center(10) for ent in res]
-    scores = [(str(round(ent['score']*100,1))+"%").center(10) for ent in res]
-    output_text += "words   :  " + "|".join(words) + "\n"
-    output_text += "groups  :  " + "|".join(groups) + "\n"
-    output_text += "scores  :  " + "|".join(scores) + "\n"
-    return output_text
+class Classifer:
+    def __init__(self):
+        config = read_config()
+        model_checkpoint = config['model']['save_dir']
+        self.token_classifier = pipeline(
+            "token-classification", model=model_checkpoint, aggregation_strategy="simple"
+        )
 
+    def _tabluar_result(self, res):
+        output_text = ""
+        words = [ent['word'].center(10) for ent in res]
+        groups = [ent['entity_group'].center(10) for ent in res]
+        scores = [(str(round(ent['score']*100,1))+"%").center(10) for ent in res]
+        output_text += "words   :  " + "|".join(words) + "\n"
+        output_text += "groups  :  " + "|".join(groups) + "\n"
+        output_text += "scores  :  " + "|".join(scores) + "\n"
+        return output_text
+
+    def __call__(self, input_text):
+        res = self.token_classifier(input_text)
+        return self._tabluar_result(res)
 
 if __name__=="__main__":
     input_text = ""
+    classifier = Classifer()
     while 1:
         input_text = input("please write a text:\n\t")
         if input_text=="q":
             break
-        res = token_classifier(input_text)
-
         print(f"the result is :")
-        print(tabluar_result(res))
+        print(classifier(input_text))
